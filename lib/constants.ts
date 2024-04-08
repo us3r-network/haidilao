e097efe9432a2cf1902d1c5cd035f67484611af3
ttang <ttang@ttang-MBP-2021.lan>
2024-04-08 15:00:10 +0800

    feat: default api url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,5 @@
-export const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const NEXT_PUBLIC_API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://api-dev.u3.xyz";
 console.log(NEXT_PUBLIC_API_BASE_URL);
 
 export const HAIDILAO_PRICE = 5;
